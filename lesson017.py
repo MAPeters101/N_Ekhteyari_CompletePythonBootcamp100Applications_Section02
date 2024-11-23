176373f9be0a5f90c3db04d22bd4ba6ae8d0098b
Mark Peters <mapeters101@earthlink.net>
2024-11-23 13:28:55 -0500

    Lesson017:  Work on lesson.

---
+++ lesson017.py
@@ -0,0 +1,17 @@
+a = bool('abc')
+b = bool(123)
+c = bool(['apples', 'cherry'])
+print(a, b, c)
+
+d = bool(False)
+e = bool(None)
+f = bool(0)
+g = bool('')
+h = bool(())
+i = bool({})
+j = bool([])
+print(d,e,f,g,h,i,j)
+
+
+
+
