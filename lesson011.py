8dcfe4c2ad6c69e15da27d8625a3e6742a002c70
Mark Peters <mapeters101@earthlink.net>
2024-11-22 23:46:10 -0500

    Lesson011:  Work on lesson.

---
+++ lesson011.py
@@ -0,0 +1,29 @@
+"""
+text :      str         x = "hello World
+
+numbers :   int         x = 20
+            float       x = 20.4
+            complex     x = 1j
+
+sequence :  list        x = ['apple', 2, 'banana']
+            tuple       x = (1, 2, 3, 4, 'name')
+            range       x = range(4)
+
+mapping :   dict        x = {'name': 'Mark', 'age': 25}
+
+set :       set         x = {1, 2, 3, 4}
+            frozenset   x = frozenset({'apple', 'cherry'})
+
+boolean :   bool        x = True
+
+binary :    bytes       x = b"hello"
+            bytearray   x = bytearray(5)
+            memoryview  x = memoryview(bytes(6))
+
+none :      None
+
+"""
+
+
+
+
