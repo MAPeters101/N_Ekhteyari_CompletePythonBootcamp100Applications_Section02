77735c6eebff1c94c8fcf70bbbce73cd2173eb84
Mark Peters <mapeters101@earthlink.net>
2024-11-22 23:34:08 -0500

    Lesson010:  Work on lesson.

---
+++ lesson010.py
@@ -0,0 +1,39 @@
+
+y = 34
+x = 5
+type_x = type(x)
+print(type_x)
+
+
+x = "5"
+type_x = type(x)
+print(type_x)
+
+x = 5
+str_x = str(x)
+type_x = type(str_x)
+print(type_x)
+print()
+
+age = 89
+Age = 11
+print(age)
+print(Age)
+print()
+
+myvar = "Mark"
+my_var = "Bob"
+_my_var = "Tim"
+myVar = "Rose"
+MYVAR = "Kim"
+myVar23 = "Lucy"
+#1myvar = "Lou"
+my3var = "Lou"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myVar23)
+print(my3var)
