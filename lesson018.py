3a8656b49aa11eb859909beb0ff455e3976328b0
Mark Peters <mapeters101@earthlink.net>
2024-11-23 13:36:39 -0500

    Lesson018:  Work on lesson.

---
+++ lesson018.py
@@ -0,0 +1,20 @@
+
+x = 1
+print(x < 5 and x < 10)
+print(x < 5 or x < 4)
+print(not(x < 4 or x < 10))
+print()
+
+
+y = x
+print(x is y)
+print(x is not y)
+
+a = [1, 2, 3]
+w = 2
+print(w in a)
+print(w not in a)
+
+
+
+
