784da4b85236aba7790ddd7b4b5a538bc4977228
Mark Peters <mapeters101@earthlink.net>
2024-11-23 12:36:26 -0500

    Lesson013:  Work on lesson.

---
+++ lesson013.py
@@ -0,0 +1,22 @@
+
+x = 'hello world!'
+print(x)
+print('hello world!!!')
+
+m = """
+hello worlds
+hello world
+hello world
+hello world
+hello world
+hello world
+
+
+"""
+print(m)
+
+print(m[0:6])
+print(m[:6])
+print(m[6:])
+
+
